6853fb9b142ebe7828cd59b42ba14638fed33713
Mark - Laptop <marksmith1702@gmail.com>
2017-11-07 22:22:21 +0000

    added JS function to make elements fade in on scroll and implemented on index & web design pages

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,21 @@ function moveToDiv(id) {
             scrollTop: $(id).offset().top
         }, 1500);
 }
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hidden').each( function(i){
+            var bottom_of_object = $(this).offset().top + ($(this).outerHeight() / 2);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){  
+                $(this).animate({'opacity':'1'},500);       
+            }   
+        }); 
+    });
+});
